c400aac11ed2a6b1fbb3d534535646b854c04522
Gerald Oliva <geraldsamoliva@gmail.com>
2024-01-04 19:49:37 -0500

    first commit

---
+++ ems-backend/src/test/java/com/geraldoliva/emsbackend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraldoliva.emsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
