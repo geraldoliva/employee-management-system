cac91b52562cba6916caf97031ee5622f5d4219d
Gerald Oliva <geraldsamoliva@gmail.com>
2024-01-21 15:34:16 -0500

    Add Header and Footer component ro React App

---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav class="navbar navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">
+            Employee Management System
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
