32289178019c6a12384710cef76110ad4f60479b
Gerald Oliva <geraldsamoliva@gmail.com>
2024-01-21 21:18:57 -0500

    Connect React App to Add Employee REST API

--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -3,26 +3,29 @@ import { listEmployees } from "../services/EmployeeService";
 import { useNavigate } from "react-router-dom";
 
 const ListEmployeeComponent = () => {
-  
   const navigator = useNavigate();
-  const [employees, setEmployees] = useState([])  
+  const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
-    listEmployees().then((response) => {
-      setEmployees(response.data)
-    }).catch(error => {
-      console.error(error)
-    })
-  }, [])
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   function addNewEmployee() {
-    navigator('/add-employee')
+    navigator("/add-employee");
   }
 
   return (
     <div className="container">
       <h2 className="text-center">List of Employees</h2>
-      <button className="btn btn-primary mb-2" onClick={addNewEmployee}>Add Employee</button>
+      <button className="btn btn-primary mb-2" onClick={addNewEmployee}>
+        Add Employee
+      </button>
       <table className="table table-striped table-bordered">
         <thead>
           <tr>
